245b5c4d3bf3a64b477a20e647170cb3d144db63
Tommy Le <tommy.le3@pcc.edu>
2025-04-19 10:38:33 -0700

    Added main method that prints Hello World!

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,10 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Created by Tommy Le 4/19/2025
+ */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
