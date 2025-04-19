b8e341f72a8a67572ed5165195b6f786ba913370
Tommy Le <tommy.le3@pcc.edu>
2025-04-19 10:40:33 -0700

    Added print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,5 @@
 package edu.pcc.cis.helloworld;
+import java.util.Date;
 
 /**
  * Created by Tommy Le 4/19/2025
@@ -6,5 +7,6 @@ package edu.pcc.cis.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
